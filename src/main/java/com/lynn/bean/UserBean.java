7784068e9836e4abe6db953e35f27581cdb2b140
lvyanqin <lvyanqin@114study.com>
2017-02-07 14:20:38 +0800

    加上jsp页面，以登录为例

---
+++ src/main/java/com/lynn/bean/UserBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lynn.bean;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UserBean {
+    
+    private Integer id;
+    private String username;
+    private String password;
+
+    public UserBean() {
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString(){
+        return "用户名：" + username + "\t 密码：" + password;
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/login_success.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : login_success
+    Created on : 2017-2-7, 14:01:54
+    Author     : Administrator
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>登录成功</title>
+    </head>
+    <body>
+        <h1>登录成功页面</h1>
+    </body>
+</html>
