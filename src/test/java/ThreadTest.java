f08d03f6bf6005112c68ba1900f7106e8df31f41
lvyanqin <1269325534@qq.com>
2017-02-21 10:29:34 +0800

    测试

---
+++ src/test/java/ThreadTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class ThreadTest {
+    
+    @Test
+    public void init(){}
+    
+    public void thread(){
+        
+    }
+    
+}
