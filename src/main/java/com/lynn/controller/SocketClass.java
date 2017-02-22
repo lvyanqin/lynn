807420e9f68ea25091baa5af87d42ad4a310760c
lvyanqin <1269325534@qq.com>
2017-02-22 15:26:49 +0800

    hibernate 失败

--- src/main/java/com/lynn/controller/SocketClass.java
+++ src/main/java/com/lynn/controller/SocketClass.java
@@ -19,43 +19,43 @@ import java.net.Socket;
  */
 public class SocketClass {
 
-    public static void main(String[] args) throws IOException {
-        InputStream is = null;
-        Socket socket = null;
-        OutputStream os = null;
-        try {
-            ServerSocket serverSocket = new ServerSocket(8080);
-            System.out.println("等待状态。。。。");
-            socket = serverSocket.accept();
-            System.out.println("处理中。。。。");
-            is = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            os = socket.getOutputStream();
-            os.write("【】你好".getBytes());
-            System.out.println("处理完毕。。。。");
-        } catch (Exception e) {
-            System.out.println("报错了。。。");
-        }finally{
-            is.close();
-            socket.close();
-        }
-    }
+//    public static void main(String[] args) throws IOException {
+//        InputStream is = null;
+//        Socket socket = null;
+//        OutputStream os = null;
+//        try {
+//            ServerSocket serverSocket = new ServerSocket(8080);
+//            System.out.println("等待状态。。。。");
+//            socket = serverSocket.accept();
+//            System.out.println("处理中。。。。");
+//            is = socket.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//            String line = "";
+//            while ((line = reader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//            os = socket.getOutputStream();
+//            os.write("【】你好".getBytes());
+//            System.out.println("处理完毕。。。。");
+//        } catch (Exception e) {
+//            System.out.println("报错了。。。");
+//        }finally{
+//            is.close();
+//            socket.close();
+//        }
+//    }
 
-    public void client() throws IOException {
-        Socket socket = new Socket("localhost", 8080);
-        InputStream is = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-        OutputStream os = socket.getOutputStream();
-        os.write("【】你好".getBytes());
-        os.close();
-    }
+//    public void client() throws IOException {
+//        Socket socket = new Socket("localhost", 8080);
+//        InputStream is = socket.getInputStream();
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        String line = "";
+//        while ((line = reader.readLine()) != null) {
+//            System.out.println(line);
+//        }
+//        OutputStream os = socket.getOutputStream();
+//        os.write("【】你好".getBytes());
+//        os.close();
+//    }
 
 }
