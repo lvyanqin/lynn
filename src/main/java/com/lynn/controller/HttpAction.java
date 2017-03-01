d55e36d33fc8edd76b28f78dff603ebdb16db411
lvyanqin <1269325534@qq.com>
2017-03-01 13:33:15 +0800

    response

--- src/main/java/com/lynn/controller/HttpAction.java
+++ src/main/java/com/lynn/controller/HttpAction.java
@@ -7,6 +7,7 @@ package com.lynn.controller;
 
 import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +29,18 @@ public class HttpAction {
     
     @RequestMapping("/header1.do")
     @ResponseBody
-    public String header1(HttpServletRequest request){
+    public String header1(HttpServletRequest request, HttpServletResponse response){
         Enumeration e = request.getHeaderNames();
         String query = request.getQueryString();//只获取get后的参数
+        String etag = request.getHeader("etag");
+        String if_none_match = request.getHeader("If-None-Match");
+        String host = request.getHeader("host");
+        String accept = request.getHeader("accept");
+        String referer = request.getHeader("referer");
+        String host1 = response.getHeader("host");
+        String etag1 = response.getHeader("etag");
+        String date1 = response.getHeader("date");
+        String server1 = response.getHeader("server");
         return query;
     }
     
