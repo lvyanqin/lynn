cc24dea2ea29b237b1a51c88fd38a7b1dc47f8af
lvyanqin <1269325534@qq.com>
2017-03-21 16:47:19 +0800

    add long test

---
+++ src/test/java/LongTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class LongTest {
+    
+    @Test
+    public void init(){}
+    
+    @Test
+    public void testLong(){
+        long L = 201605031522L;
+        System.out.println("【】" + (L + 1) );
+    }
+}
