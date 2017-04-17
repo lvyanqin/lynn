30643f838ed32577855609d9a3581b1b4edda927
lvyanqin <lvyanqin@114study.com>
2017-04-17 15:49:26 +0800

    test

--- src/main/java/com/lynn/bean/UserBean.java
+++ src/main/java/com/lynn/bean/UserBean.java
@@ -5,64 +5,232 @@
  */
 package com.lynn.bean;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
+ * 用户
  *
- * @author Administrator
  */
-public class UserBean {
-    
-    private Integer id;
+@Entity
+@Table(name = "user")
+public class UserBean implements Serializable {
+
+    private static final long serialVersionUID = -3894075644880531845L;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "salt")
+    private String salt;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "ip")
+    private String ip;
+
+    @Column(name = "createTime")
+    private Integer createTime;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "descript")
+    private String descript;
+
+    @Column(name = "idCard")
+    private String idCard;
+
+    @Column(name = "mobile")
+    private String mobile;
+
+    @Column(name = "state")
+    private Integer state;
+
+    @Column(name = "identity")
+    private Integer indentity;
+
+    @Column(name = "mobileVaildate")
+    private Integer mobileVaildate;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "emailValidate")
+    private Integer emailValidate;
+
+    @Column(name = "avatar")
+    private Integer avatar;
+
+    @Column(name = "points")
+    private Integer points;// 积分数
+
     public UserBean() {
+
     }
 
-    /**
-     * @return the id
-     */
     public Integer getId() {
-        return id;
+        return Id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(Integer id) {
-        this.id = id;
+        Id = id;
     }
 
-    /**
-     * @return the username
-     */
     public String getUsername() {
         return username;
     }
 
-    /**
-     * @param username the username to set
-     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    /**
-     * @return the password
-     */
     public String getPassword() {
         return password;
     }
 
-    /**
-     * @param password the password to set
-     */
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Integer createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Integer getIndentity() {
+        return indentity;
+    }
+
+    public void setIndentity(Integer indentity) {
+        this.indentity = indentity;
+    }
+
+    public Integer getMobileVaildate() {
+        return mobileVaildate;
+    }
+
+    public void setMobileVaildate(Integer mobileVaildate) {
+        this.mobileVaildate = mobileVaildate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getEmailValidate() {
+        return emailValidate;
+    }
+
+    public void setEmailValidate(Integer emailValidate) {
+        this.emailValidate = emailValidate;
+    }
+
+    public Integer getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Integer avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "用户名：" + username + "\t 密码：" + password;
     }
-    
+
 }
